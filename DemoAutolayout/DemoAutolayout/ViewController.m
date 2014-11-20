99105c3ebd522d9b38f1aa8d07bd0f6525dfa33f
chancier <306965810@qq.com>
2014-11-20 18:08:54 +0800

    测试@2x  @3x

--- DemoAutolayout/DemoAutolayout/ViewController.m
+++ DemoAutolayout/DemoAutolayout/ViewController.m
@@ -18,9 +18,46 @@
 
 @implementation ViewController
 
+-(UIImage*) imagePathed:(NSString*)imageName
+{
+    NSString *strPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:imageName];
+    UIImage *img = [UIImage imageWithContentsOfFile:strPath];
+    
+    if ( nil == img )
+    {
+        img = [UIImage imageNamed:imageName];
+    }
+    
+    return img;
+}
+
+/*
+ *
+ 如果@2x、@3x都存在
+｛
+    6以及一下自动取@2x图片
+    6+取@3x图片
+ ｝
+ 
+ 以下是取了各个图片后的处理方式：
+ 
+ @2x： @2x全部自动除以2 无论是否带了后缀@2x.png
+ @3x： @3x全部自动除以3 无论是否带了后缀@3x.png
+ 
+ 
+ 建议：不带@2x @3x 
+ 这样  6以及一下自动取@2x图片
+       6+取@3x图片
+ 
+ *
+ */
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImage *image =[self imagePathed:@"banner_1.png"];
+//    [self imagePathed:@"banner_1@3x.png"];
+    NSLog(@"image%f %f",image.size.width,image.size.height);
     
     dataArr = [NSArray arrayWithObjects:@"sfafaxxCC",@"afasxcvasfasfsadfasdfsdfsadfasdfasdfasdfsadfsdfasdfasxcvasfasfsadfasdfsdfsadfasdfasdfasdfsadfsdfasdfsasxcvasfasfsadfasdfsdfsadfasdfasdfasdfsadfsdfasdfssd",@"afasxcvasfasfsadfasdfsdfsadfasdfasdfasdfsadfsdfasdfasxcvasfasfsadfasdfsdfsadfasdfasdfasdfsadfsdfasdfsasxcvasfsadfasdfasdfasdfsadfsdfasdfasxcvasfasfsadfasdfsdfsadfasdfasdfasdfsadfsdfasdfsasxcvasfasfsadasfsadfasdfsdfsadfasdfasdfasdfsadfsdfasdfssd", nil];
     
